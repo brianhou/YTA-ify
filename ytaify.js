6135bc30f2be30af36310c5bf53f08bd81d644ce
brianhou <brian.hou@live.com>
2013-03-17 03:09:52 -0700

    first chrome extension!

---
+++ ytaify.js
@@ -0,0 +1,8 @@
+var watch_re = /\?v=(\w+)&?/;
+function youtube_check(tabID, changeInfo, tab) {
+    var result = watch_re.exec(tab.url);
+    if (result != null) {
+        chrome.tabs.update(tab.id, {url:"http://www.ytadventure.com/#" + result[1]});
+    }
+}
+chrome.tabs.onUpdated.addListener(youtube_check);
