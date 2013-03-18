8b1e1f50c7b9fb5324eead2fa1b3834236a2319d
brianhou <brian.hou@live.com>
2013-03-18 01:36:20 -0700

    oops, forgot to check if we're on youtube. implementing event pages will make this safer though.

--- YTA-ify/ytaify.js
+++ YTA-ify/ytaify.js
@@ -1,8 +1,8 @@
-console.log("Installing...");
 var watch_re = /v=([-\w]+)&?/;
 function youtube_check(tabID, changeInfo, tab) {
     var result = watch_re.exec(tab.url);
-    if (result != null) {
+    // quick fix before transitioning to event pages
+    if ((tab.url.indexOf("www.youtube.com/") >= 0) && (result != null)) {
         chrome.tabs.update(tab.id, {url:"http://www.ytadventure.com/#" + result[1]});
     }
 }
