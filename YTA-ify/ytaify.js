0036a68076f56d3e873846911fbee61395f38cc6
brianhou <brian.hou@live.com>
2014-01-09 03:57:28 -0800

    add button to end adventure

--- YTA-ify/ytaify.js
+++ YTA-ify/ytaify.js
@@ -1,15 +1,34 @@
+// YouTube to YTAdventure
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
     if (tab.url.indexOf("www.youtube.com/") >= 0) {
-        chrome.pageAction.show(tabId); // only show pageAction on YouTube
+        chrome.pageAction.show(tabId);
     }
 });
 
 chrome.pageAction.onClicked.addListener(function(tab) {
-    // Fires when the icon is clicked.
-    var watch_re = /v=([-\w]+)&?/, result = watch_re.exec(tab.url);
-    if (result != null) {
-        chrome.tabs.update(tab.id,
-                           {url: "http://www.ytadventure.com/#" + result[1]});
+    if (tab.url.indexOf("www.youtube.com/") >= 0) {
+        var video_id = /v=([-\w]+)&?/.exec(tab.url);
+        if (video_id != null) {
+            var new_url = "http://ytadventure.com/#" + video_id[1];
+            chrome.tabs.update(tab.id, {url: new_url});
+        }
+    }
+});
+
+// YTAdventure to YouTube -- this probably won't be needed.
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+    if (tab.url.indexOf("ytadventure.com/") >= 0) {
+        chrome.pageAction.show(tabId);
+    }
+});
+
+chrome.pageAction.onClicked.addListener(function(tab) {
+    if (tab.url.indexOf("ytadventure.com/") >= 0) {
+        var video_id = /ytadventure.com\/#([-\w]+)/.exec(tab.url);
+        if (video_id != null) {
+            var new_url = "http://youtube.com/watch?v=" + video_id[1];
+            chrome.tabs.update(tab.id, {url: new_url});
+        }
     }
 });
 
