8e87aaf57d3b424438ea50a6a1bcbeb488fa8903
brianhou <brian.hou@live.com>
2014-01-09 03:26:18 -0800

    add button to turn on the adventure!

--- YTA-ify/ytaify.js
+++ YTA-ify/ytaify.js
@@ -1,10 +1,16 @@
-var watch_re = /v=([-\w]+)&?/;
-function youtube_check(tabID, changeInfo, tab) {
-    var result = watch_re.exec(tab.url);
-    // quick fix before transitioning to event pages
-    if ((tab.url.indexOf("www.youtube.com/") >= 0) && (result != null)) {
-        chrome.tabs.update(tab.id, {url:"http://www.ytadventure.com/#" + result[1]});
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+    if (tab.url.indexOf("www.youtube.com/") >= 0) {
+        chrome.pageAction.show(tabId); // only show pageAction on YouTube
     }
-}
-chrome.tabs.onUpdated.addListener(youtube_check);
+});
+
+chrome.pageAction.onClicked.addListener(function(tab) {
+    // Fires when the icon is clicked.
+    var watch_re = /v=([-\w]+)&?/, result = watch_re.exec(tab.url);
+    if (result != null) {
+        chrome.tabs.update(tab.id,
+                           {url: "http://www.ytadventure.com/#" + result[1]});
+    }
+});
+
 console.log("Ready to adventure!");
